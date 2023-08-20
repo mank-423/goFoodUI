17a4d57b78b03499cd78aeae1e2494e92cc3721e
Mayank <mk4664348@gmail.com>
2023-08-21 00:13:38 +0530

    API links changed for production

--- src/Screens/MyOrder.jsx
+++ src/Screens/MyOrder.jsx
@@ -9,7 +9,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/myOrderData", {
+    await fetch("https://gofoodapi-i32n.onrender.com/api/myOrderData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
